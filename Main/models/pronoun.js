3c1422d6aabbb2910016716ebce13d4689fdf916
Daniel Bradley <danielbvideo@gmail.com>
2021-09-15 19:54:18 -0400

    updated to pronoun from gender

---
+++ Main/models/pronoun.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create a new Sequelize model for books
+class Pronoun extends Model {}
+
+Pronoun.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement:true,
+    },
+    name: {
+      type: DataTypes.STRING
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: 'pronoun'
+  }
+
+);
+
+module.exports = Pronoun;
