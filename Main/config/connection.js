ee53e05c3f3c8f544b812e01caa0a0d456d6a670
Daniel Bradley <danielbvideo@gmail.com>
2021-09-17 14:38:52 -0400

    connected handlebars, updated routes, tested database, and ran server

--- Main/config/connection.js
+++ Main/config/connection.js
@@ -1,10 +1,10 @@
-//const Sequelize = require('sequelize');
-//let sequelize;
-//require('dotenv').config();
-//
-//if(process.env.JAWSDB_URL){
+// const Sequelize = require('sequelize');
+// let sequelize;
+// require('dotenv').config();
+
+// if(process.env.JAWSDB_URL){
 //    sequelize = new Sequelize(process.env.JAWSDB_URL);
-//} else {
+// } else {
 //    sequelize = new Sequelize (
 //        process.env.DB_NAME,
 //        process.env.DB_USER,        
@@ -15,9 +15,9 @@
 //            port: 3306,
 //        },          
 //    );
-//}
+// }
 
-//module.exports = sequelize;
+// module.exports = sequelize;
 
 
  //Daniel used the following to test today
